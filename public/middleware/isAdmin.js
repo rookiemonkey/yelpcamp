4c04fbc20baba66949245ad0960457a857bc7002
Kevin Roi Basina <kevinroirigorbasina@pm.me>
2020-06-27 21:25:44 +0800

    fixed bug ADMIN not showing on navbar for admin logins

--- public/middleware/isAdmin.js
+++ public/middleware/isAdmin.js
@@ -1,13 +1,11 @@
 const isAdmin = (req, next) => {
-    if(req.header.cookie) {
-        const c = req.headers.cookie;
-        const i = c.indexOf('role=');
-        if(i == -1) {
-            return false
-        } else {
-            return true
-        }
-    } return false
+    const c = req.headers.cookie;
+    const i = c.indexOf('role=');
+    if(i == -1) {
+        return false
+    } else {
+        return true
+    }
 }
 
 module.exports = isAdmin;
