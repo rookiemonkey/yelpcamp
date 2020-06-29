78cc17ef4379647b2f716dda20b5ea914fb5c323
Kevin Roi Basina <kevinroirigorbasina@pm.me>
2020-06-29 08:30:11 +0800

    refactored multer/cloudinary configuration

--- public/middleware/toUpload.js
+++ public/middleware/toUpload.js
@@ -1,11 +1,11 @@
 const toUpload = (cloudinary, req) => {
     const promise = new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(req.file.path, function(result) {
-            if(!result) { reject(error) }
+        cloudinary.uploader.upload(req.file.path, function (result) {
+            if (!result) { reject(error) }
             else { resolve(result) }
         })
     })
-    promise.then(result => { console.log(result); return result })
+    promise.then(result => { return result })
     promise.catch(error => { return error })
     return promise;
 }
--- public/routes/new.js
+++ public/routes/new.js
@@ -9,109 +9,91 @@ const cloudinary = require('cloudinary');
 const isAdmin = require("../middleware/isAdmin");
 const isLoggedIn = require("../middleware/isLoggedin");
 const toUpload = require("../middleware/toUpload");
+const setMulter = require("../middleware/setMulter");
+const setCloudinary = require("../middleware/setCloudinary");
 const Campground = require("../schemas/campgroundSchema");
 
 // configure multer
-const storage = multer.diskStorage({
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  }
-});
-
-const imageFilter = function (req, file, cb) {
-    // accept image files only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
-};
-
-const upload = multer({ storage: storage, fileFilter: imageFilter})
-
+const upload = setMulter(multer);
 
 // configure cloudinary
-cloudinary.config({
-  cloud_name: 'promises',
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
+cloudinary.config(setCloudinary());
 
 // ===========================
 // NEW ROUTE
 // ===========================
 router.get("/campgrounds/new", isLoggedIn, (req, res) => {
-    if(isAdmin(req)) {
-        res.render("addcampground", {
-            user: req.user,
-            role: "ADMIN"
-        });
-    } else {
-        res.render("addcampground", {
-            user: req.user,
-            role: null
-        });
-    }
+  if (isAdmin(req)) {
+    res.render("addcampground", {
+      user: req.user,
+      role: "ADMIN"
+    });
+  } else {
+    res.render("addcampground", {
+      user: req.user,
+      role: null
+    });
+  }
 });
 
 // ===========================
 // CREATE ROUTE: handler for new campgrounds
 // ===========================
 router.post("/campgrounds/new", isLoggedIn, upload.single('image'), async (req, res) => {
-    try {
-        const uploaded = await toUpload(cloudinary, req);
-        const image = uploaded.secure_url;
-        const campname = req.sanitize(req.body.name);
-        const location = req.sanitize(req.body.location);
-        const description = req.sanitize(req.body.description);
-        const uploader = { id: req.user.id, name: req.user.username}
-        let lat, lng, formattedLocation;
+  try {
+    const uploaded = await toUpload(cloudinary, req);
+    const image = uploaded.secure_url;
+    const campname = req.sanitize(req.body.name);
+    const location = req.sanitize(req.body.location);
+    const description = req.sanitize(req.body.description);
+    const uploader = { id: req.user.id, name: req.user.username }
+    let lat, lng, formattedLocation;
 
-        await opencage.geocode({ q: location })
-            .then(res => { return JSON.stringify(res)})
-            .then(data => {
-                const d = JSON.parse(data)
-              if (d.status.code == 200) {
-                if (d.results.length > 0) {
-                  lat = d.results[0].geometry.lat;
-                  lng = d.results[0].geometry.lng;
-                  formattedLocation = d.results[0].formatted;
-                }
-              } else if (d.status.code == 402) {
-                console.log('hit free-trial daily limit');
-                console.log('become a customer: https://opencagedata.com/pricing');
-              } else {
-                console.log('error', d.status.message);
-              }
-            })
-            .catch(error => {
-              console.log('error', error.message);
-            });
+    await opencage.geocode({ q: location })
+      .then(res => { return JSON.stringify(res) })
+      .then(data => {
+        const d = JSON.parse(data)
+        if (d.status.code == 200) {
+          if (d.results.length > 0) {
+            lat = d.results[0].geometry.lat;
+            lng = d.results[0].geometry.lng;
+            formattedLocation = d.results[0].formatted;
+          }
+        } else if (d.status.code == 402) {
+          console.log('hit free-trial daily limit');
+          console.log('become a customer: https://opencagedata.com/pricing');
+        } else {
+          console.log('error', d.status.message);
+        }
+      })
+      .catch(error => {
+        console.log('error', error.message);
+      });
 
-        const newCampground = {
-            campname: campname,
-            price: req.body.price,
-            image: image,
-            location: formattedLocation,
-            lat: lat,
-            lng: lng,
-            description: description,
-            uploader: uploader
-        };
+    const newCampground = {
+      campname: campname,
+      price: req.body.price,
+      image: image,
+      location: formattedLocation,
+      lat: lat,
+      lng: lng,
+      description: description,
+      uploader: uploader
+    };
 
-        Campground.create(newCampground, (err, addedCamp) => {
-            if (err) {
-                req.flash("error", err.message)
-                res.redirect("/campgrounds");
-            } else {
-                req.flash("success", `Congratulations ${req.user.username}! Your new camp "${addedCamp.campname}" is now added on our catalogue`)
-                res.redirect("/campgrounds");
-            };
-        });
-    } catch  {
-        req.flash('error', `Something went wrong upon creating the campground. ${err}`)
-        res.redirect('/campgrounds/new');
-    }
+    Campground.create(newCampground, (err, addedCamp) => {
+      if (err) {
+        req.flash("error", err.message)
+        res.redirect("/campgrounds");
+      } else {
+        req.flash("success", `Congratulations ${req.user.username}! Your new camp "${addedCamp.campname}" is now added on our catalogue`)
+        res.redirect("/campgrounds");
+      };
+    });
+  } catch  {
+    req.flash('error', `Something went wrong upon creating the campground. ${err}`)
+    res.redirect('/campgrounds/new');
+  }
 });
 
 // ===========================
