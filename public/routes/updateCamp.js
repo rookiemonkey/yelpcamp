2cbfa99a67c2bc2a06bf55bd28ae601213d1d068
Kevin Roi Basina <kevinroirigorbasina@pm.me>
2020-06-29 10:00:15 +0800

    refactored updatecamp route for geocoding

--- public/routes/updateCamp.js
+++ public/routes/updateCamp.js
@@ -5,52 +5,36 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const isAdmin = require("../middleware/isAdmin");
-const opencage = require('opencage-api-client');
+const toGeocode = require("../middleware/toGeocode");
 const Campground = require("../schemas/campgroundSchema");
 
 // =========================================
 // UPDATE ROUTE: edit handler
 // =========================================
-router.put("/campgrounds/:id/update",( req, res) => {
-    Campground.findById(req.params.id, async (err, foundCampground) => {
-        if(req.session.passport !== undefined && foundCampground.uploader.id.equals(req.user.id) || isAdmin(req)) {
+router.put("/campgrounds/:id/update", (req, res) => {
+  Campground.findById(req.params.id, async (err, foundCampground) => {
+    if (req.session.passport !== undefined && foundCampground.uploader.id.equals(req.user.id) || isAdmin(req)) {
 
-            await opencage.geocode({ q: req.body.updates.location })
-                .then(res => { return JSON.stringify(res)})
-                .then(data => {
-                    const d = JSON.parse(data)
-                  if (d.status.code == 200) {
-                    if (d.results.length > 0) {
-                      req.body.updates.lat = d.results[0].geometry.lat;
-                      req.body.updates.lng = d.results[0].geometry.lng;
-                      req.body.updates.location = d.results[0].formatted;
-                    }
-                  } else if (d.status.code == 402) {
-                    console.log('hit free-trial daily limit');
-                    console.log('become a customer: https://opencagedata.com/pricing');
-                  } else {
-                    console.log('error', d.status.message);
-                  }
-                })
-                .catch(error => {
-                  console.log('error', error.message);
-                });
+      const loc = await toGeocode(req.body.updates.location);
+      req.body.updates.lat = loc.lat;
+      req.body.updates.lng = loc.lng;
+      req.body.updates.location = loc.formattedLocation;
 
-            Campground.findByIdAndUpdate(req.params.id, req.body.updates, (err) => {
-                if(err) {
-                    req.flash("error", `Something went wrong upon updating. ${err.message} Please try again`);
-                    res.redirect(`/campgrounds/${req.params.id}`);
-                } else {
-                    req.flash("success", `"${foundCampground.campname}" was updated successfully`);
-                    res.redirect(`/campgrounds/${req.params.id}`);
-                }
-            });
+      Campground.findByIdAndUpdate(req.params.id, req.body.updates, (err) => {
+        if (err) {
+          req.flash("error", `Something went wrong upon updating. ${err.message} Please try again`);
+          res.redirect(`/campgrounds/${req.params.id}`);
         } else {
-            // if not logged in let them log in first
-            req.flash("error", "Something is not right. You need to be logged in and should be the owner of the camp to edit it");
-            res.redirect("/campgrounds/login");
+          req.flash("success", `"${foundCampground.campname}" was updated successfully`);
+          res.redirect(`/campgrounds/${req.params.id}`);
         }
-    })
+      });
+    } else {
+      // if not logged in let them log in first
+      req.flash("error", "Something is not right. You need to be logged in and should be the owner of the camp to edit it");
+      res.redirect("/campgrounds/login");
+    }
+  })
 });
 
 
