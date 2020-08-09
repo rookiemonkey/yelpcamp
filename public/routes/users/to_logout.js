fc118d1f1078e10068c1d8d5e9450abff9e42f39
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-09 19:40:20 +0800

    login/signup middlewares working

--- public/routes/users/to_logout.js
+++ public/routes/users/to_logout.js
@@ -1,12 +1,4 @@
 // ===========================
-// ROUTE DEPENDENCIES
-// ===========================
-const express = require("express");
-const router = express.Router();
-
-// MIDDLEWARE: isLoggedIn
-
-// ===========================
 // LOGOUT ROUTE:
 // ===========================
 const to_logout = (req, res) => {
@@ -19,39 +11,6 @@ const to_logout = (req, res) => {
     });
 
 };
-
-
-// middleware - check if signed in
-function isLoggedIn(req, res, next) {
-
-    // req.session.passport contains an object when user is signed
-    // if not, req.session.passport is undefined
-    let rt = req.route;
-    let rs = req.session;
-
-    if (rs.passport !== undefined) {
-
-        if (rt.path === "/campgrounds/users/signup" && rt.hasOwnProperty("methods")) {
-
-            res.redirect("/campgrounds/camps");
-
-        } else {
-
-            return next();
-
-        }
-
-    } else if (rs.passport === undefined && rt.methods.post === true) {
-
-        return next();
-
-    } else {
-
-        res.redirect("/campgrounds/users/login");
-
-    }
-};
-
 
 // ===========================
 // EXPORTS ALL THE ROUTES
