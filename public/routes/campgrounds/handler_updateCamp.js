898c75eb24593bb2c56fe4bec7f02e5d26908566
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-15 17:08:54 +0800

    update post working

--- public/routes/campgrounds/handler_updateCamp.js
+++ public/routes/campgrounds/handler_updateCamp.js
@@ -13,31 +13,31 @@ cloudinary.config(setCloudinary());
 // =========================================
 // UPDATE ROUTE: edit handler
 // =========================================
-const handler_updateCamp = (req, res) => {
-  Campground.findById(req.params.id, async (err, foundCampground) => {
-    if (req.session.passport !== undefined && foundCampground.uploader.id.equals(req.user.id) || isAdmin(req)) {
+const handler_updateCamp = async (req, res) => {
 
-      const { image_default } = req.body;
-      const imageUrl = await toUpload(cloudinary, req)
-      const { lat, lng, formattedLocation } = await toGeocode(req.body.location);
-      let image = imageUrl ? imageUrl : image_default
-      const updates = { ...req.body, lat, lng, location: formattedLocation, image }
+  try {
+    const foundCampground = await Campground.findById(req.params.id)
 
-      Campground.findByIdAndUpdate(req.params.id, updates, (err) => {
-        if (err) {
-          req.flash("error", `Something went wrong upon updating. ${err.message} Please try again`);
-          res.redirect(`/campgrounds/camps/${req.params.id}`);
-        } else {
-          req.flash("success", `"${foundCampground.campname}" was updated successfully`);
-          res.redirect(`/campgrounds/camps/${req.params.id}`);
-        }
-      });
-    } else {
-      // if not logged in let them log in first
-      req.flash("error", "Something is not right. You need to be logged in and should be the owner of the camp to edit it");
-      res.redirect("/campgrounds/users/login");
+    if (req.session.passport === undefined &&
+      !foundCampground.uploader.id.equals(req.user.id) || isAdmin(req)) {
+      throw new Error('You need to be logged in and should be the owner of the camp to edit it')
     }
-  })
+
+    const { image_default } = req.body;
+    const { lat, lng, formattedLocation } = await toGeocode(req.body.location);
+    const imageUrl = await toUpload(cloudinary, req)
+    const image = imageUrl ? imageUrl : image_default
+    const updates = { ...req.body, lat, lng, location: formattedLocation, image }
+
+    await Campground.findByIdAndUpdate(req.params.id, updates)
+    req.flash("success", `"${foundCampground.campname}" was updated successfully`);
+    res.redirect(`/campgrounds/camps/${req.params.id}`);
+  }
+
+  catch (error) {
+    req.flash("error", `Something is not right. ${error.message}`);
+    res.redirect(`/campgrounds/camps/${req.params.id}`);
+  }
 };
 
 
