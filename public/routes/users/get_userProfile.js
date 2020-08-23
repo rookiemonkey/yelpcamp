fd66b1ac2dc33e95f127239dae4857043f52c93b
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-23 21:50:45 +0800

    added user reviews on user profile

--- public/routes/users/get_userProfile.js
+++ public/routes/users/get_userProfile.js
@@ -3,6 +3,7 @@
 // ===========================
 const User = require("../../schemas/userSchema");
 const Campground = require("../../schemas/campgroundSchema");
+const Review = require('../../schemas/reviewSchema');
 const isAdmin = require("../../middleware/isAdmin");
 
 // ===========================
@@ -18,19 +19,34 @@ const user_profile = async (req, res) => {
             .where('uploader.id')
             .equals(foundUser._id)
 
+        const userReviews = await Review
+            .find({})
+            .where('author._id')
+            .equals(foundUser._id)
+
+        const foundUserReviews = await Promise.all(
+            userReviews.map(async review => {
+                const options = `_id campname image location`
+                const foundCamp = await Campground.findById(review.campground, options)
+                const { rating, text, createdAt, _id } = review
+                return { rating, text, createdAt, _id, campground: foundCamp }
+            }))
+
         if (isAdmin(req)) {
             res.render("userProfile", {
                 user: req.user,
                 role: "ADMIN",
                 foundUser,
-                foundUserCamps
+                foundUserCamps,
+                foundUserReviews
             });
         } else {
             res.render("userProfile", {
                 user: req.user,
                 role: null,
                 foundUser,
-                foundUserCamps
+                foundUserCamps,
+                foundUserReviews
             });
         }
     }
