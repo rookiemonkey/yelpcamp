6c8fedabe37bdc4d6c2cc4c0932cc7aae391d304
rookiemonkey <kevinroirigorbasina@pm.me>
2020-10-08 20:09:29 +0800

    for deployment

---
+++ env.import copy.sh
@@ -0,0 +1,8 @@
+
+heroku config:set DBURL="" &&
+heroku config:set PASSPORT_SECRET="" &&
+heroku config:set CLOUDINARY_API_KEY="" &&
+heroku config:set CLOUDINARY_API_SECRET="" &&
+heroku config:set GMAILUN="@gmail.com" &&
+heroku config:set GMAILPW="" &&
+heroku config:set OCD_API_KEY=""
